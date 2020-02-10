e654d0503d4466c0801317784ec3cc9fe4a5a4ad
feralonsomaccari <feralonsomaccari@gmail.com>
2020-02-09 21:39:59 -0300

    Add full responsiveness to big screens

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const changeTheme = () => {
   const body = document.querySelector(".wrapper");
+  const header = document.querySelector(".header");
   const btn = document.querySelector(".change-theme");
   const bodyClasses = [...body.classList];
   if (bodyClasses.some(className => className === "body--dark")) {
     body.classList.remove("body--dark");
+    // header.classList.remove('header--dark');
     btn.innerHTML = "🌛";
   } else {
     body.classList.add("body--dark");
+    // header.classList.add('header--dark');
     btn.innerHTML = "🌞";
   }
 };
@@ -25,13 +28,15 @@ const generateSidePorject = repository => {
     if (repository.homepage != null) {
       project_demo.innerHTML = "Live Demo";
       project_demo.href = repository.homepage;
+      project_demo.classList.add("link");
       project_nav.append(project_demo);
     }
+    project_github.classList.add("link");
     project.classList.add("project");
     project_github.innerHTML = "Github";
     project_github.href = repository.html_url;
     project_nav.append(project_github);
-    project_title.innerHTML = repository.name + " →";
+    project_title.innerHTML = repository.name;
     project_desc.innerHTML = repository.description;
     project.append(project_title);
     project.append(project_nav);

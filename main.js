5ffffd85a5221a41caad8d1f53c1d89fd3f0330b
maccarif <fernando.maccari@barrons.com>
2025-08-10 18:54:49 +0200

    feat: new styles

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const handleThemeToggle = () => {
   const body = document.querySelector(".wrapper");
   const texts = document.querySelectorAll(".text")
   const links = document.querySelectorAll(".link")
-  const emoji = document.querySelector("#emoji-theme");
+  const sections = document.querySelectorAll(".section")
   const LampAnimation = document.querySelector(".lamp");
   const bodyClasses = [...body.classList];
+
   if (bodyClasses.some(className => className === "body--dark")) {
     body.classList.remove("body--dark");
-    emoji.setAttribute("aria-label", "moon");
-    emoji.innerHTML = "🌛"
+    document.documentElement.style.background = "white";
+    body.classList.remove("body--dark");
   } else {
+    document.documentElement.style.background = "rgb(25, 25, 25)";
     body.classList.add("body--dark");;
-    emoji.setAttribute("aria-label", "sun");
-    emoji.innerHTML = "🌞"
   }
 
   texts.forEach(texts => {
@@ -27,6 +27,12 @@ const handleThemeToggle = () => {
       ? links.classList.remove("link--dark")
       : links.classList.add("link--dark")
   });
+  sections.forEach(links => {
+    const textClasses = [...links.classList];
+    textClasses.some(className => className === "section--dark")
+      ? links.classList.remove("section--dark")
+      : links.classList.add("section--dark")
+  });
 
   [...LampAnimation.classList].some(className => className === "lamp--dark")
     ? LampAnimation.classList.remove("lamp--dark")
@@ -34,56 +40,56 @@ const handleThemeToggle = () => {
 
 };
 
-const generateSideProject = repository => {
-  if (repository.size < 500 || repository.fork) return;
-  const wrapper = document.querySelector(".projects__wrapper");
-
-  const project = document.createElement("article");
-  const project_title = document.createElement("h3");
-  const project_desc = document.createElement("p");
-  const project_nav = document.createElement("nav");
-  const project_demo = document.createElement("a");
-  const project_github = document.createElement("a");
-  const project_lang = document.createElement("div");
-
-  if (repository.homepage) {
-    project_demo.innerHTML = "Live Demo";
-    project_demo.href = repository.homepage;
-    project_demo.classList.add("link");
-    project_demo.target = "_blank";
-    project_demo.rel = "noopener noreferrer";
-    project_nav.append(project_demo);
-  }
-
-  project.classList.add("project");
-  project_github.classList.add("link");
-  project_github.target = "_blank";
-  project_github.rel = "noopener noreferrer";
-  project_github.innerHTML = "Github";
-  project_github.href = repository.html_url;
-  project_nav.append(project_github);
-  project_title.innerHTML = repository.name;
-  project_desc.innerHTML = repository.description;
-  project_desc.classList.add("text")
-  const topicArray = repository.topics;
-  topicArray.forEach((topic) => {
-    const topicEl = document.createElement("span");
-    topicEl.classList.add("topic")
-    topicEl.textContent = `${topic}`;
-    project_lang.append(topicEl)
-  })
-  project.append(project_title);
-  project.append(project_nav);
-  project.append(project_desc);
-  project.append(project_lang);
-  wrapper.append(project);
-};
+// const generateSideProject = repository => {
+//   if (repository.size < 500 || repository.fork) return;
+//   const wrapper = document.querySelector(".projects__wrapper");
+//
+//   const project = document.createElement("article");
+//   const project_title = document.createElement("h3");
+//   const project_desc = document.createElement("p");
+//   const project_nav = document.createElement("nav");
+//   const project_demo = document.createElement("a");
+//   const project_github = document.createElement("a");
+//   const project_lang = document.createElement("div");
+//
+//   if (repository.homepage) {
+//     project_demo.innerHTML = "Live Demo";
+//     project_demo.href = repository.homepage;
+//     project_demo.classList.add("link");
+//     project_demo.target = "_blank";
+//     project_demo.rel = "noopener noreferrer";
+//     project_nav.append(project_demo);
+//   }
+//
+//   project.classList.add("project");
+//   project_github.classList.add("link");
+//   project_github.target = "_blank";
+//   project_github.rel = "noopener noreferrer";
+//   project_github.innerHTML = "Github";
+//   project_github.href = repository.html_url;
+//   project_nav.append(project_github);
+//   project_title.innerHTML = repository.name;
+//   project_desc.innerHTML = repository.description;
+//   project_desc.classList.add("text")
+//   const topicArray = repository.topics;
+//   topicArray.forEach((topic) => {
+//     const topicEl = document.createElement("span");
+//     topicEl.classList.add("topic")
+//     topicEl.textContent = `${topic}`;
+//     project_lang.append(topicEl)
+//   })
+//   project.append(project_title);
+//   project.append(project_nav);
+//   project.append(project_desc);
+//   project.append(project_lang);
+//   wrapper.append(project);
+// };
 
 
 function triggerFireworks(event) {
   const fireworks = document.querySelector('.fireworks');
   const numParticles = 20;
-  const colors = ['#ff0', '#ff6347', '#ff1493', '#00f', '#32cd32']; 
+  const colors = ['#ff0', '#ff6347', '#ff1493', '#00f', '#32cd32'];
 
   const { top, left, width, height } = event.target.getBoundingClientRect();
   const centerX = left + width / 2;
@@ -94,34 +100,36 @@ function triggerFireworks(event) {
     const distance = Math.random() * 100 + 50;
     const x = Math.cos(angle) * distance;
     const y = Math.sin(angle) * distance;
-    
+
     const particle = document.createElement('div');
     particle.classList.add('particle');
-    
+
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     particle.style.backgroundColor = randomColor;
-    
+
     particle.style.left = `${centerX + window.scrollX - 3}px`;
     particle.style.top = `${centerY + window.scrollY - 3}px`;
     particle.style.setProperty('--x', `${x}px`);
     particle.style.setProperty('--y', `${y}px`);
-    
+
     fireworks.appendChild(particle);
-    
+
     setTimeout(() => particle.remove(), 1000);
   }
 }
-const getRepositories = () => {
-  fetch("https://api.github.com/users/feralonsomaccari/repos")
-    .then(response => response.json())
-    .then(response => {
-      response.sort((a, b) => a.size - b.size)
-      response.map(repository => generateSideProject(repository));
-    })
-    .catch(error => {
-      console.error(error)
-      // In case it fail it will show a hand wrote side project section that can be outdated
-    });
-};
-getRepositories();
-document.querySelector(".theme-toggle-btn").addEventListener("click", handleThemeToggle);
+// const getRepositories = () => {
+//   fetch("https://api.github.com/users/feralonsomaccari/repos")
+//     .then(response => response.json())
+//     .then(response => {
+//       response.sort((a, b) => a.size - b.size)
+//       response.map(repository => generateSideProject(repository));
+//     })
+//     .catch(error => {
+//       console.error(error)
+//       // In case it fail it will show a hand wrote side project section that can be outdated
+//     });
+// };
+// getRepositories();
+
+document.querySelector(".toggle").addEventListener("click", handleThemeToggle);
+

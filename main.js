d38eebbe20f9fee46d13ba05761f04c2345fce2d
maccarif <fernando.maccari@barrons.com>
2026-04-22 11:59:05 +0200

    refactor: clean css and js code

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ const handleThemeToggle = () => {
   const LampAnimation = document.querySelector(".lamp");
   const bodyClasses = [...body.classList];
 
+  const lightBgColor = "#fff"
+  const darkBgColor = "#191919"
+
   if (bodyClasses.some(className => className === "body--dark")) {
     body.classList.remove("body--dark");
-    document.documentElement.style.background = "white";
+    document.documentElement.style.background = lightBgColor;
     body.classList.remove("body--dark");
   } else {
-    document.documentElement.style.background = "rgb(25, 25, 25)";
+    document.documentElement.style.background = darkBgColor;
     body.classList.add("body--dark");;
   }
 
@@ -39,52 +42,6 @@ const handleThemeToggle = () => {
     : LampAnimation.classList.add("lamp--dark")
 
 };
-
-// const generateSideProject = repository => {
-//   if (repository.size < 500 || repository.fork) return;
-//   const wrapper = document.querySelector(".projects__wrapper");
-//
-//   const project = document.createElement("article");
-//   const project_title = document.createElement("h3");
-//   const project_desc = document.createElement("p");
-//   const project_nav = document.createElement("nav");
-//   const project_demo = document.createElement("a");
-//   const project_github = document.createElement("a");
-//   const project_lang = document.createElement("div");
-//
-//   if (repository.homepage) {
-//     project_demo.innerHTML = "Live Demo";
-//     project_demo.href = repository.homepage;
-//     project_demo.classList.add("link");
-//     project_demo.target = "_blank";
-//     project_demo.rel = "noopener noreferrer";
-//     project_nav.append(project_demo);
-//   }
-//
-//   project.classList.add("project");
-//   project_github.classList.add("link");
-//   project_github.target = "_blank";
-//   project_github.rel = "noopener noreferrer";
-//   project_github.innerHTML = "Github";
-//   project_github.href = repository.html_url;
-//   project_nav.append(project_github);
-//   project_title.innerHTML = repository.name;
-//   project_desc.innerHTML = repository.description;
-//   project_desc.classList.add("text")
-//   const topicArray = repository.topics;
-//   topicArray.forEach((topic) => {
-//     const topicEl = document.createElement("span");
-//     topicEl.classList.add("topic")
-//     topicEl.textContent = `${topic}`;
-//     project_lang.append(topicEl)
-//   })
-//   project.append(project_title);
-//   project.append(project_nav);
-//   project.append(project_desc);
-//   project.append(project_lang);
-//   wrapper.append(project);
-// };
-
 
 function triggerFireworks(event) {
   const fireworks = document.querySelector('.fireworks');
@@ -117,19 +74,6 @@ function triggerFireworks(event) {
     setTimeout(() => particle.remove(), 1000);
   }
 }
-// const getRepositories = () => {
-//   fetch("https://api.github.com/users/feralonsomaccari/repos")
-//     .then(response => response.json())
-//     .then(response => {
-//       response.sort((a, b) => a.size - b.size)
-//       response.map(repository => generateSideProject(repository));
-//     })
-//     .catch(error => {
-//       console.error(error)
-//       // In case it fail it will show a hand wrote side project section that can be outdated
-//     });
-// };
-// getRepositories();
 
 document.querySelector(".toggle").addEventListener("click", handleThemeToggle);
 
